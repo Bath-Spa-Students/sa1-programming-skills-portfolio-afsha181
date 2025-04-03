b8ff3df0049145cb7c7ee067ae0dd3d2f4a139d6
Moaz-Ali777 <gafsha101@gmail.com>
2025-04-03 14:46:54 -0700

    Primitive Quiz Advanced.py

---
+++ Exercise 04/Primitive Quiz Advanced.py
@@ -0,0 +1,59 @@
+country = input("WHAT IS THE CAPITAL OF FRANCE? ")
+if country.lower() == "paris":
+    print("your answer is correct!")
+else:
+    print("your answer is wrong.")
+
+country = input("WHAT IS THE CAPITAL OF AUSTRIA? ")
+if country.lower() == "vienna":
+    print("your answer is correct!")
+else:
+    print("your answer is wrong.")
+
+country = input("WHAT IS THE CAPITAL OF CROATIA? ")
+if country.lower() == "zagreb":
+    print("your answer is correct!")
+else:
+    print("your answer is wrong.")
+
+country = input("WHAT IS THE CAPITAL OF HUNGARY? ")
+if country.lower() == "budapest":
+    print("your answer is correct!")
+else:
+    print("your answer is wrong.")
+
+country = input("WHAT IS THE CAPITAL OF UKRAINE? ")
+if country.lower() == "kyiv":
+    print("your answer is correct!")
+else:
+    print("your answer is wrong.")
+
+country = input("WHAT IS THE CAPITAL OF POLAND? ")
+if country.lower() == "warsaw":
+    print("your answer is correct!")
+else:
+    print("your answer is wrong.")
+
+country = input("WHAT IS THE CAPITAL OF PORTUGAL? ")
+if country.lower() == "lisbon":
+    print("your answer is correct!")
+else:
+    print("your answer is wrong.")
+
+country = input("WHAT IS THE CAPITAL OF DENMARK? ")
+if country.lower() == "copenhagen":
+    print("your answer is correct!")
+else:
+    print("your answer is wrong.")
+
+country = input("WHAT IS THE CAPITAL OF SWEDEN? ")
+if country.lower() == "stockholm":
+    print("your answer is correct!")
+else:
+    print("your answer is wrong.")
+
+country = input("WHAT IS THE CAPITAL OF ROMANIA? ")
+if country.lower() == "bucharest":
+    print("your answer is correct!")
+else:
+    print("your answer is wrong.")
