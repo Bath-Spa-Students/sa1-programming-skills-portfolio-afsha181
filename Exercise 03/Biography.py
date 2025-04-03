a440616b6e1a1d07bd6ffb1158207b5d8b264e8c
Moaz-Ali777 <gafsha101@gmail.com>
2025-04-03 14:44:27 -0700

    Biography.py

---
+++ Exercise 03/Biography.py
@@ -0,0 +1,6 @@
+personaldetails = {
+    "name": "Afsha",
+    "hometown": "Mayasaloon",
+    "age": 18
+}
+print(f"Name: {personaldetails['name']}\nHometown: {personaldetails['hometown']}\nAge: {personaldetails['age']}")
