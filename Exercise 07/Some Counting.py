fe145578ad49ba0e2f1d85615a842363950785c8
Moaz-Ali777 <gafsha101@gmail.com>
2025-04-03 14:59:59 -0700

    Some counting.py

---
+++ Exercise 07/Some Counting.py
@@ -0,0 +1,48 @@
+#using increment of 1
+print("counting up from 0 to 50:")
+i = 0
+while i <= 50:
+    print(i, end=", ")
+    i += 1
+print('''
+''')
+
+
+#using increment of 1
+print("counting down from 50 to 0 :")
+i = 50
+while i >= 0:
+    print(i, end=", ")
+    i -= 1
+print('''
+''')
+
+
+#using increment of 1
+print("counting up from 30 to 50 :")
+i = 30
+while i <= 50:
+    print(i, end=", ")
+    i += 1
+print('''
+''')
+
+
+
+#using increment of 2
+print("counting down from 30 to 50 :")
+i = 30
+while i <= 50:
+    print(i, end=", ")
+    i += 2
+print('''
+''')
+
+#using increment of 5
+print("counting up from 100 to 200 :")
+i = 100
+while i <= 200:
+    print(i, end=", ")
+    i += 5
+print('''
+''')
